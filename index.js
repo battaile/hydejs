1200cb4fa4594bfe7585e8e214ad979bb96af5f4
Battaile Fauber <battaile@gmail.com>
2015-05-25 17:49:28 -0400

    getting a functional list of post links on the title page

--- index.js
+++ index.js
@@ -6,27 +6,41 @@ var sourcePostsDirectory = target + '/posts/';
 var targetDirectory = target + '/dist/';
 var targetPostsDirectory = targetDirectory + 'posts/';
 
-var files = fs.readdirSync(sourcePostsDirectory);
+var files = fs.readdirSync(sourcePostsDirectory).sort();
 
 files.forEach(processSourceFiles);
 createHomePage();
 
 function createHomePage() {
-  var html = "<a target='_blank' href='posts/cummings-v-gomez-adcc-trials.html'>first post (this wont really be hardcoded)</a>";
-  fs.writeFileSync(targetDirectory + 'index.html', html);
+
+  var mwd = '';
+  var txtFiles = files.filter(function(x){return isMwd(x);});
+
+  for (var i = 0; i < txtFiles.length; i++){
+    mwd += '[' + txtFiles[i].replace(/-/g,' ').replace('.mwd','') + '](posts/' + txtFiles[i].replace(/(\.mwd)/,'.html') + ')<br/>';
+  }
+  console.log(mwd);
+  fs.writeFileSync(targetDirectory + 'index.html', getHtmlFullPage(mwd));
 }
 
+// refactor homepage generation to work off arrays getting built when processign source files so it can access the data there
 function processSourceFiles(element, index, array) {
   console.log("processing " + element);
   var sourcePath = sourcePostsDirectory + element;
   var targetPath = targetPostsDirectory + element.replace(/(\.mwd)/,'.html');
 
-  if (/^.*(\.mwd)/.test(element)){
+  if (isMwd(element)){
     processMwdFile(sourcePath, targetPath);
   }
   if (/^.*(\.png)/.test(element)){
     copyImage(sourcePath, targetPath);
   }
+}
+
+function isMwd(file){
+  var result = /^.*(\.mwd)/.test(file);
+  console.log('isMwd result for ' + file + ' is ' + result);
+  return result;
 }
 
 function copyImage(sourcePath, targetPath){
